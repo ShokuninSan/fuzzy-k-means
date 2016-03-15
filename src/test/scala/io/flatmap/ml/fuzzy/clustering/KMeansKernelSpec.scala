f84c362de33be036afdfc9c3e192640fcaf1e7ab
Shokunin San <me@flatmap.io>
2016-03-15 10:38:45 +0100

    Added test demoing internals of normalize method.

--- src/test/scala/io/flatmap/ml/fuzzy/clustering/KMeansKernelSpec.scala
+++ src/test/scala/io/flatmap/ml/fuzzy/clustering/KMeansKernelSpec.scala
@@ -1,8 +1,8 @@
 package io.flatmap.ml.fuzzy.clustering
 
-import breeze.linalg.DenseMatrix
+import breeze.linalg.{sum, Axis, DenseMatrix}
 import org.scalatest.{FlatSpec, Matchers}
-import io.flatmap.ml.fuzzy.functions.eps
+import io.flatmap.ml.fuzzy.functions._
 
 class KMeansKernelSpec extends FlatSpec with Matchers {
 
@@ -69,6 +69,23 @@ class KMeansKernelSpec extends FlatSpec with Matchers {
       (0.375, 0.125)
     )
     assert(normalized == expected)
+  }
+
+  "product of a unit matrix of shape (n x 1) and a sum matrix of shape (1 x m)" should "produce a (n x m) matrix with n copies of the sums" in {
+    val n = 3
+    val m = 5
+    val ones = unitMatrix(rows = n, cols = 1)
+    val sums = DenseMatrix.create(rows = 1, cols = 5, Array(2.0, 4.0, 6.0, 8.0, 10.0))
+
+    val product = ones * sums
+
+    val expected = DenseMatrix(
+      (2.0, 4.0, 6.0, 8.0, 10.0),
+      (2.0, 4.0, 6.0, 8.0, 10.0),
+      (2.0, 4.0, 6.0, 8.0, 10.0)
+    )
+
+    assert(product == expected)
   }
 
 }
